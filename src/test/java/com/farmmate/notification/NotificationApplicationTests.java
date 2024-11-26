12d71434fd37ba3fd58689899622eab957a3ff44
swandevson <shai4956@gmail.com>
2024-11-27 01:32:53 +0900

    [FM-113] :white_check_mark: test container + docker compose 테스트코드 작성

--- src/test/java/com/farmmate/notification/NotificationApplicationTests.java
+++ src/test/java/com/farmmate/notification/NotificationApplicationTests.java
@@ -1,12 +1,21 @@
 package com.farmmate.notification;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Import(TestcontainersConfiguration.class)
+@Testcontainers
 @SpringBootTest
 class NotificationApplicationTests {
+	@Container
+	static DockerComposeContainer<?> composeContainer =
+		new DockerComposeContainer<>(new File("compose.yaml"))
+			.withExposedService("mysql", 3306)
+			.withExposedService("redis", 6379);
 
 	@Test
 	void contextLoads() {
